46e29596fe9cceea35e78723b6216143e43eb3d0
Klauß <123550461+klau55@users.noreply.github.com>
2025-04-02 15:26:43 +0300

    Update db.ts

--- clicker-backend/src/db.ts
+++ clicker-backend/src/db.ts
@@ -5,7 +5,7 @@ const dbConfig: PoolConfig = {
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'clickerdb',
-  password: process.env.DB_PASSWORD || 'NDKJB-UQJMP-4VL8W-XBLQX-MPCNR',
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   // Add connection pool settings
   max: 20, // Maximum number of clients in the pool
